ca4274beb88a3342343b382add2db3da8703c860
Shereen <shereenfinney@gmail.com>
2016-11-26 21:24:19 +0000

    2nd commit - ex2.rb

---
+++ ex2.rb
@@ -0,0 +1,17 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+# aka  octothorpe, pound, hash
+
+puts "I could have code like this." # and the comment is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "this won't run"
+
+puts "this will run"
+
+# review each line going backward.
+# --------------------------------
+# Start at the last line, and check each word in reverse against what you should have typed.
+# It's a trick to make your brain not attach meaning to each part of the code,
+# and doing that makes you process each piece exactly.
+# This catches errors and is a handy error-checking technique.
